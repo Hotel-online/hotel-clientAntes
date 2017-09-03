c3ab27aa1a1f0d7a520f55a6689459937805c8f9
paulneo <localos2000@gmail.com>
2017-09-03 13:57:12 -0500

    iniciando home hotel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,12 @@
 import React, { Component } from 'react';
-import Footer from './home/Footer';
-import Hom from './home/Hom';
+
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { title: "react redux" };
-    }
-
-    handleClick = (e) => {
-        console.log('this is:', e.target.value)
-        this.setState({
-            title: e.target.value
-        })
-    }
 
     render() {
         return (
-            <div className="Home">
-                <Hom title={this.state.title} />
-                <div className="Content">
-                    <input type="text" onChange={this.handleClick} />
-                </div>
-                <Footer />
+            <div className="img">
+
             </div>
         );
     }
