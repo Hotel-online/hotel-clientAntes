ce1804bab1a0415affaa6677da75afa37b8f83c8
RamirezJosue <xdemobtkx@gmail.com>
2017-09-03 19:09:17 -0500

    logo

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -9,15 +9,9 @@ import './Header.css';
 class Header extends Component {
     render() {
         return (
-            <nav>
-			<ul>
-				<li><NavLink exact to="/" activeClassName="selected">Home</NavLink></li>
-                <li><NavLink exact to="/abouts" activeClassName="selected">Buscar cuarto</NavLink></li>
-                <li><NavLink exact to="/one" activeClassName="selected">one</NavLink></li>
-                <li><NavLink exact to="/counters" activeClassName="selected">Counter</NavLink></li>
-                <li><NavLink exact to="/users" activeClassName="selected">users</NavLink></li>
-			</ul>
-		</nav>
+            <div>
+                Logo
+                </div>
         );
     }
 }
