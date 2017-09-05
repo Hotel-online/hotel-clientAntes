7dc046db52b16825148a871fd349b8e5e1deb8ba
paulneo <localos2000@gmail.com>
2017-09-05 08:45:00 -0500

    git josn merge

--- src/actions/roomsAction.js
+++ src/actions/roomsAction.js
@@ -5,7 +5,7 @@ export const REPLACE_PRODUCTS = 'REPLACE_PRODUCTS'
 const loadProducts = () => {
   return dispatch => {
     console.log("loadProducts");
-    return axios.get("https://lp2-20172.github.io/hotel-client/rooms.json")
+    return axios.get("http://localhost:3000/rooms.json")
       .then(response => {
         console.log(response.data);
         dispatch({ type: REPLACE_PRODUCTS, products: response.data.rooms })
