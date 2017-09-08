73e97d000826de93723e2b7b462f4013c9d032f7
paulneo <localos2000@gmail.com>
2017-09-08 16:25:51 -0500

    añadiendo logo

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -6,22 +6,16 @@ import {
     NavLink
 
 } from 'react-router-dom'
-import './Header.css';
+import '../style.css';
 
 class Header extends Component {
     render() {
         return (
             <nav>
                 <ul>
-                    <li><NavLink exact to="/" activeClassName="selected">Home</NavLink></li>
+                    <li><NavLink exact to="/" activeClassName="selected">Inicio</NavLink></li>
                     <li><NavLink exact to="rooms" activeClassName="selected">Habitaciones</NavLink></li>
                     <li><NavLink exact to="photos" activeClassName="selected">Fotos</NavLink></li>
-                    <li><NavLink exact to="/abouts" activeClassName="selected">abouts</NavLink></li>
-                    <li><NavLink exact to="/one" activeClassName="selected">one</NavLink></li>
-                    <li><NavLink exact to="/counters" activeClassName="selected">Counter</NavLink></li>
-                    <li><NavLink exact to="/users" activeClassName="selected">users</NavLink></li>
-                    <li><NavLink exact to="/footer" activeClassName="selected">Footer</NavLink></li>
-
                 </ul>
             </nav>
         );
