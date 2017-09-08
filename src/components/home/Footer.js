8ff03d69b96aca52e721d1e072f00a5c84be8494
paulneo <localos2000@gmail.com>
2017-09-08 17:30:28 -0500

    centrando footer

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
-            <div className="Footer">
-                <div>
+            <div className="footer">
+                <div className="footer_seccion">
                   <h3>HOTEL 5 ESTRELLAS</h3>
                   <p>Con fabulosos cuartos te ofrecemos una buena atención</p>
                 </div>
-                <div>
+                <div className="footer_seccion">
                   <h3>CONTACTENOS</h3>
                   <p>Av.Arequipa</p>
                   <p>Celular: 957463521</p>
