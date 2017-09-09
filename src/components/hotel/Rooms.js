0efd060a0145a9be546da637c7e7933adb0a5bdc
RamirezJosue <xdemobtkx@gmail.com>
2017-09-09 10:00:01 -0500

    implementando rooms

--- src/components/hotel/Rooms.js
+++ src/components/hotel/Rooms.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react';
 class Rooms extends Component {
     render() {
         return (
-            <div className="Footer">
-                Hola soy Rooms Implementar
+            <div class="card">
+            <header class="card__thumb">
+                <a href="#">
+                <img src="" alt=""/>
+                </a>
+            </header>
+            <div class="card__body">
+                <div class="card__category"><a href="#">Photos</a></div>
+                <h2 class="card__title"><a href="#">Reservar</a></h2>
+                <div class="card__subtitle">Habitacione Doble</div>
+                <p class="card__description">
+                Mas Informacion
+                </p>
+            </div>
             </div>
         );
     }
