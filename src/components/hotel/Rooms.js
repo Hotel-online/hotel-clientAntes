da16b79c1423eabe2cbf049d745f2bd50f84b6a5
RamirezJosue <xdemobtkx@gmail.com>
2017-09-09 17:02:26 -0500

    Actualizando habitaciones 2

--- src/components/hotel/Rooms.js
+++ src/components/hotel/Rooms.js
@@ -58,6 +58,57 @@ class Rooms extends Component {
                                 </div>
                             </div>
                         </li>
+                        <li className="property-boxes col-xs-6 col-md-4">
+                            <div className="prp-img">
+                                <img src="https://www.pioletspark.com/content/imgsxml/galerias/panel_habitaciones/6/tab-0016-pioletspark-doblepremium566.jpg" alt=""/>
+                                <div className="price"><span>20</span></div>   
+                            </div>
+                            <div className="prp-detail">
+                                <div className="title">
+                                    Habitacion Doble
+                                </div>
+                                <div className="description">
+                                    Informacion
+                                    <a className="more-detail btn colored" >
+                                        Reservar
+                                    </a>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="property-boxes col-xs-6 col-md-4">
+                            <div className="prp-img">
+                                <img src="https://www.pioletspark.com/content/imgsxml/galerias/panel_habitaciones/6/tab-0016-pioletspark-doblepremium566.jpg" alt=""/>
+                                <div className="price"><span>20</span></div>   
+                            </div>
+                            <div className="prp-detail">
+                                <div className="title">
+                                    Habitacion Doble
+                                </div>
+                                <div className="description">
+                                    Informacion
+                                    <a className="more-detail btn colored" >
+                                        Reservar
+                                    </a>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="property-boxes col-xs-6 col-md-4">
+                            <div className="prp-img">
+                                <img src="https://www.pioletspark.com/content/imgsxml/galerias/panel_habitaciones/6/tab-0016-pioletspark-doblepremium566.jpg" alt=""/>
+                                <div className="price"><span>20</span></div>   
+                            </div>
+                            <div className="prp-detail">
+                                <div className="title">
+                                    Habitacion Doble
+                                </div>
+                                <div className="description">
+                                    Informacion
+                                    <a className="more-detail btn colored" >
+                                        Reservar
+                                    </a>
+                                </div>
+                            </div>
+                        </li>
                     </ul>
                 </section>
             </div>
