e2c6f5066c81744fed45546fb4f500634fd3783c
rotkyobel <rotkyobel@gmail.com>
2017-09-10 09:29:23 -0500

    formulario-reserva

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 class Form extends Component {
     render() {
         return (
-            <div>
-                About
+            <div className="imgbackground portada">
+              <div className="reserva">
+                <h2>RESERVA</h2>
+                  <input className="form2" type="text"  placeholder="Nombre"></input>
+                  <input className="form2" type="text"  placeholder="Apellido"></input>
+                  <input className="form2" type="text"  placeholder="DNI"></input>
+                  <input className="form2" type="date"  name="bday" min="2000-01-02"></input>
+                  <input className="form2" type="date"  name="bday" min="2000-01-02"></input>
+                  <input className="form2" type="email" placeholder="E-mail"></input>
+                  <input className="form2 btn"  type="button" name="aceptar" value="Aceptar"/>
+              </div>
+
             </div>
         );
     }
