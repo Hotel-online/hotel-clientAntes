4ace640398e358e538c6d9eed419901188820b13
paulneo <localos2000@gmail.com>
2017-09-10 00:15:18 -0500

    añadiendo rgba a mi texto

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,8 +12,8 @@ class Home extends Component {
     render() {
         return (
           <div className="imgbackground portada" >
-            <div>
-              <input className="form" type="date"  name="bday" min="2000-01-02">
+            <div className="formulario">
+              <input className="form" type="date"  name="bday" min="2000-01-02" >
               </input>
               <input className="form" type="date"  name="bday" min="2000-01-02">
               </input>
@@ -24,10 +24,13 @@ class Home extends Component {
                 <option value="Matrimonial">Matrimonial</option>
                 <option value="Triple">Triple</option>
               </select>
-              <NavLink exact to="/rooms"  activeClassName="selected"><input className="form"  type="submit" name="" value="RESERVE SU CUARTO"/></NavLink>
+              <NavLink exact to="/rooms"  activeClassName="selected"><input className="form"  type="submit" name="" value="RESERVAR"/></NavLink>
 
             </div>
-            <h1>Te ayudamos a que pases una noche increible en nuestras fabulosas habitaciones</h1>
+            <div className="text">
+              <h1>Te ayudamos a que pases una noche increible en nuestras fabulosas habitaciones</h1>
+            </div>
+
 
           </div>
 
