3c61ff0e0d2fa29638b44976d07c9cc0d2745c72
paulneo <localos2000@gmail.com>
2017-09-11 19:37:26 -0500

    agregando background img en mi login

--- src/components/logins/Login.js
+++ src/components/logins/Login.js
@@ -11,21 +11,21 @@ import {
 class Login extends Component {
     render() {
         return (
-        	 <div className="Cuerpo_Principal" >
-            <div className="formulario_lo">>
-                <form className="cuerpoform">
-                	<label className="texxto">Usuario</label>
-                	<input type="email"/>
-                	<label className="texxto">Contraseña</label>
-		<input type="password"/>
-		<a className="bot_ol" href="/recuppasswords">¿Has olvidado tu Contraseña?</a>
-		<NavLink exact to="/bienvenidos"  activeClassName="selected">
-               	<input className=" form2 btn"  type="submit" name="" value="Iniciar Sesión"/></NavLink>
-		<a id="botonhref">Entrar con: <a href="www.facebook.com">Facebook</a>/  /<a href="www.google.com">Google</a></a>
-               	<NavLink exact to="/forms"  activeClassName="selected">
-               	<input className="form2 btn"  type="submit" name="" value="Registrarse"/></NavLink>
-                </form>
-            </div>
+        	 <div className="imgbackground" >
+              <div className="formulario_lo">>
+                  <form className="cuerpoform">
+                    	<label className="texxto">Usuario</label>
+                    	<input type="email"/>
+                    	<label className="texxto">Contraseña</label>
+                  		<input type="password"/>
+                  		<a className="bot_ol" href="/recuppasswords">¿Has olvidado tu Contraseña?</a>
+                  		<NavLink exact to="/bienvenidos"  activeClassName="selected">
+                                 	<input className=" form2 btn"  type="submit" name="" value="Iniciar Sesión"/></NavLink>
+                  		<a id="botonhref">Entrar con: <a href="www.facebook.com">Facebook</a>/  /<a href="www.google.com">Google</a></a>
+                     	<NavLink exact to="/forms"  activeClassName="selected">
+                   	<input className="form2 btn"  type="submit" name="" value="Registrarse"/></NavLink>
+                  </form>
+              </div>
            </div>
         );
     }
