ff0f38c046a2afe1c982700a4ca113ec96d7bfbf
paulneo <localos2000@gmail.com>
2017-09-11 19:56:01 -0500

    centrando fomulario

--- src/components/logins/Login.js
+++ src/components/logins/Login.js
@@ -12,18 +12,19 @@ class Login extends Component {
     render() {
         return (
         	 <div className="imgbackground" >
-              <div className="formulario_lo">>
+              <div className="formulario_lo">
                   <form className="cuerpoform">
-                    	<label className="texxto">Usuario</label>
-                    	<input type="email"/>
-                    	<label className="texxto">Contraseña</label>
-                  		<input type="password"/>
-                  		<a className="bot_ol" href="/recuppasswords">¿Has olvidado tu Contraseña?</a>
-                  		<NavLink exact to="/bienvenidos"  activeClassName="selected">
-                                 	<input className=" form2 btn"  type="submit" name="" value="Iniciar Sesión"/></NavLink>
+                    	<label >Usuario</label>
+                    	<input className="form form_login" type="email" placeholder="E-mail"/>
+                    	<label >Contraseña</label>
+                  		<input className="form form_login" type="password" placeholder="Password"/>
+                  		<NavLink exact to="/bienvenidos"  activeClassName="selected"><input className=" form form_login"  type="submit" name="" value="Iniciar Sesión"/></NavLink>
+                      <NavLink exact to="/forms"  activeClassName="selected"><input className="form form_login"  type="submit" name="" value="Registrarse"/></NavLink>
+                      <a className="bot_ol" href="/recuppasswords">¿Has olvidado tu Contraseña?</a>
+
                   		<a id="botonhref">Entrar con: <a href="www.facebook.com">Facebook</a>/  /<a href="www.google.com">Google</a></a>
-                     	<NavLink exact to="/forms"  activeClassName="selected">
-                   	<input className="form2 btn"  type="submit" name="" value="Registrarse"/></NavLink>
+
+
                   </form>
               </div>
            </div>
